6929e061c8d4375221abc73ebc38524d7c6cdf07
Adam Cooper <adam.cooper@gmail.com>
2013-09-03 00:13:21 -0700

    add pry as development dependency

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'tempfile'
 require 'hipchat'
 require 'gist'
 require 'playbah' # and any other gems you need
+require 'pry'
 
 RSpec.configure do |config|
     # some (optional) config here
